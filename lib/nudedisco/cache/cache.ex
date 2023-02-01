19de03f3c54db0a8878cef9facd03be2c4ecb7d2
Shola Anozie <shola@soulprovidr.fm>
2023-01-31 21:45:03 -0500

    Added cache layer to improve response time. (#3)

---
+++ lib/nudedisco/cache/cache.ex
@@ -0,0 +1,22 @@
+defmodule Nudedisco.Cache do
+  import Cachex.Spec
+
+  @cache_table :cache
+
+  def child_spec(_init_arg) do
+    %{
+      id: @cache_table,
+      type: :supervisor,
+      start:
+        {Cachex, :start_link,
+         [
+           @cache_table,
+           [
+             warmers: [
+               warmer(module: Nudedisco.Cache.Warmer, state: nil)
+             ]
+           ]
+         ]}
+    }
+  end
+end
