4defc617a4ef4e3f25447c6b8461850a88d48ed4
Shola Anozie <shola@soulprovidr.fm>
2023-11-19 17:44:41 -0500

    Now using the priv/ directory for more than public files.

--- lib/nudedisco/web/controllers/static.ex
+++ lib/nudedisco/web/controllers/static.ex
@@ -2,7 +2,8 @@ defmodule Nudedisco.Web.Controllers.Static do
   use Raxx.SimpleServer
 
   defp read_file_from_path(path) do
-    file_path = Path.join(:code.priv_dir(:nudedisco), Path.join(path))
+    public_dir = Path.join(:code.priv_dir(:nudedisco), "public")
+    file_path = Path.join(public_dir, Path.join(path))
 
     with {:ok, file} <- File.read(file_path) do
       mime_type = MIME.from_path(file_path)
