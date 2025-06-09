1accad5e0f2faab04c6cbf43044a70dc541630ed
Shola Anozie <shola@soulprovidr.fm>
2025-06-09 10:35:23 -0400

    Adjust deployment script for railway.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /app
+
 _build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.create_db"
 _build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.migrate"
 _build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.populate_feeds"
