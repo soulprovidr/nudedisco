7d053ee021b28de341d33a1bcba0fc58595de591
Shola Anozie <shola@soulprovidr.fm>
2025-06-09 10:39:05 -0400

    Another attempt to fix the deployment command.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /app
-
-_build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.create_db"
-_build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.migrate"
-_build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.populate_feeds"
-_build/prod/rel/nudedisco/bin/nudedisco start
+/app/_build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.create_db"
+/app/_build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.migrate"
+/app/_build/prod/rel/nudedisco/bin/nudedisco eval "Nudedisco.Release.populate_feeds"
+/app/_build/prod/rel/nudedisco/bin/nudedisco start
